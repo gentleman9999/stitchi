59c49adf17d1bad2335458b4a5ec66fcb30b1984
Everest Guerra <everest@umich.edu>
2022-01-09 22:02:18 -0500

    stashing

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  mode: 'jit',
+  future: {
+    purgeLayersByDefault: true,
+    applyComplexClasses: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        'primary-light': 'var(--primary-light)',
+        'primary-dark': 'var(--primary-dark)',
+        secondary: 'var(--secondary)',
+        'secondary-2': 'var(--secondary-2)',
+        hover: 'var(--hover)',
+        'hover-1': 'var(--hover-1)',
+        'hover-2': 'var(--hover-2)',
+        'accent-0': 'var(--accent-0)',
+        'accent-1': 'var(--accent-1)',
+        'accent-2': 'var(--accent-2)',
+        'accent-3': 'var(--accent-3)',
+        'accent-4': 'var(--accent-4)',
+        'accent-5': 'var(--accent-5)',
+        'accent-6': 'var(--accent-6)',
+        'accent-7': 'var(--accent-7)',
+        'accent-8': 'var(--accent-8)',
+        'accent-9': 'var(--accent-9)',
+        violet: 'var(--violet)',
+        'violet-light': 'var(--violet-light)',
+        'violet-dark': 'var(--violet-dark)',
+        pink: 'var(--pink)',
+        'pink-light': 'var(--pink-light)',
+        cyan: 'var(--cyan)',
+        blue: 'var(--blue)',
+        green: 'var(--green)',
+        red: 'var(--red)',
+        paper: 'var(--paper)',
+      },
+
+      textColor: {
+        base: 'var(--text-base)',
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+      },
+    },
+  },
+}
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,13 +1,4 @@
 module.exports = {
+  presets: [require('../config/tailwind.config.js')],
   content: ['./**/*.{js,ts,jsx,tsx}'],
-  mode: 'jit',
-  future: {
-    purgeLayersByDefault: true,
-    applyComplexClasses: true,
-  },
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
 }
