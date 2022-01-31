398d365e31bfb7d73d408f04c9dbae886556da0e
Everest Guerra <everest@umich.edu>
2022-01-31 15:35:33 -0500

    push

--- apps/marketing/src/components/pages/HomePage/HomePageSimpleFeatureSection.tsx
+++ apps/marketing/src/components/pages/HomePage/HomePageSimpleFeatureSection.tsx
@@ -1,22 +1,28 @@
 import { Section } from '@components/common'
-import { DesktopComputer, Doodle1, MapPin } from 'icons'
+import Image from 'next/image'
 import React from 'react'
+import CustomIcon from '../../../../public/merch/custom-store.svg'
+import DesignIcon from '../../../../public/merch/design.svg'
+import LogisticsIcon from '../../../../public/merch/logistics.svg'
 
 const features = [
   {
-    name: 'Professional, high-quality custom merchandise',
-    description: 'Free consultation and design',
-    icon: Doodle1,
+    name: 'Professional, quality custom merchandise',
+    description:
+      'Work with a designer to turn your ideas into beautiful merchandise people love.',
+    icon: DesignIcon,
   },
   {
     name: 'Tailored to your unique needs',
-    description: 'High-quality customization on premium products',
-    icon: DesktopComputer,
+    description:
+      'From simple swag bulk-ordering to fully integrated merch programs, our solutions scale with your needs.',
+    icon: CustomIcon,
   },
   {
     name: 'Delivered throughout the world ',
-    description: 'Automated warehousing and fulfillment solutions',
-    icon: MapPin,
+    description:
+      'Our logistics solutions ensure you never have to think about packing, storing, or shipping merchandise ever again.',
+    icon: LogisticsIcon,
   },
 ]
 
@@ -28,21 +34,22 @@ const HomePageSimpleFeatureSection = () => {
           Promotional Product Experts
         </h2>
         <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          Create promotional products people will love
+          Create Promotional products people love
         </p>
         <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-          Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-          voluptatum cupiditate veritatis in accusamus quisquam.
+          We are a team of promotional product designers, marketers, and coders
+          at your disposal to make your next merch project a breeze.
         </p>
       </div>
       <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8 mt-10">
         {features.map(feature => (
           <div key={feature.name}>
             <dt>
-              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-primary">
-                <feature.icon
-                  className="h-6 w-6 stroke-secondary"
-                  aria-hidden="true"
+              <div className="relative flex items-center justify-center h-40 w-40 rounded-md bg-paper">
+                <Image
+                  {...feature.icon}
+                  layout="fill"
+                  alt={`${feature.name} icon`}
                 />
               </div>
               <p className="mt-5 text-lg leading-6 font-medium text-gray-900">
