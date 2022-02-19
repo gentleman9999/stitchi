bd5f9c9bf93b16dfc88e0c10df76d8b2580ad7a4
Everest Guerra <everest@umich.edu>
2022-02-19 01:13:51 -0500

    footer patch

--- apps/marketing/src/components/layout/PrimaryLayout/PrimaryLayout.tsx
+++ apps/marketing/src/components/layout/PrimaryLayout/PrimaryLayout.tsx
@@ -7,7 +7,7 @@ export interface PrimaryLayoutProps {}
 const PrimaryLayout: React.FC<PrimaryLayoutProps> = props => {
   const { children } = props
   return (
-    <div className="flex flex-col justify-between h-screen">
+    <div className="flex flex-col justify-between min-h-screen">
       <Navbar />
       <main className="mb-auto relative overflow-x-hidden">{children}</main>
       <Footer />
