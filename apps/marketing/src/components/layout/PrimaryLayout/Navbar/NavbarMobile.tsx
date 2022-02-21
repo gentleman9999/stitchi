c52ffe645cb57083b60dae8f22cfeab8dc26d773
Everest Guerra <everest@umich.edu>
2022-02-20 20:24:04 -0500

    test nav

--- apps/marketing/src/components/layout/PrimaryLayout/Navbar/NavbarMobile.tsx
+++ apps/marketing/src/components/layout/PrimaryLayout/Navbar/NavbarMobile.tsx
@@ -1,7 +1,6 @@
-import { ChevronDown, HamburgerMenu } from 'icons'
+import { HamburgerMenu } from 'icons'
 import React from 'react'
 import Link from 'next/link'
-import cx from 'classnames'
 import s from './NavbarMobile.module.css'
 import { Navigation } from '@lib/navigation'
 import useDropdown from './useDropdown'
@@ -19,15 +18,10 @@ interface Props {
 const NavbarMobile = ({ anchorEl, navigation }: Props) => {
   const { active: dialogOpen, handleClick: handleOpenDialog } =
     useDropdown<boolean>()
-  const { active: dropdownActive, handleClick: handleDropdownClick } =
-    useDropdown()
-
-  const solutionsLabel = 'Why Stitchi?'
-  const resourcesLabel = 'Resources'
 
   return (
     <div>
-      <button onClick={() => handleOpenDialog(true)}>
+      <button className="p-2" onClick={() => handleOpenDialog(true)}>
         <HamburgerMenu height={24} width={24} />
       </button>
       <NavbarDropdown
@@ -48,6 +42,7 @@ const NavbarMobile = ({ anchorEl, navigation }: Props) => {
         <div className={s.item}>
           <Link href={routes.internal.getStarted.href()} passHref>
             <Button
+              bold
               shadow
               Component="a"
               color="brandPrimary"
@@ -57,79 +52,7 @@ const NavbarMobile = ({ anchorEl, navigation }: Props) => {
             </Button>
           </Link>
         </div>
-
-        {/* <div>
-          <div className={s.item} key={solutionsLabel}>
-            <button
-              className={s.link}
-              onClick={() => handleDropdownClick(solutionsLabel)}
-            >
-              {solutionsLabel}
-              <span
-                className={cx('ml-2 transition', {
-                  'transform rotate-180': dropdownActive === solutionsLabel,
-                })}
-              >
-                <ChevronDown />
-              </span>
-            </button>
-            <Dropdown expanded={dropdownActive === solutionsLabel}>
-              {navigation.solutions.map(subItem => (
-                <Link href={subItem.href} key={subItem.label}>
-                  <a className="block mb-2 text-lg text-secondary">
-                    {subItem.label}
-                  </a>
-                </Link>
-              ))}
-            </Dropdown>
-          </div>
-          <div className={s.item} key={resourcesLabel}>
-            <button
-              className={s.link}
-              onClick={() => handleDropdownClick(resourcesLabel)}
-            >
-              {resourcesLabel}
-              <span
-                className={cx('ml-2 transition', {
-                  'transform rotate-180': dropdownActive === resourcesLabel,
-                })}
-              >
-                <ChevronDown />
-              </span>
-            </button>
-            <Dropdown expanded={dropdownActive === resourcesLabel}>
-              {navigation.resources.map(subItem => (
-                <Link href={subItem.href} key={subItem.label}>
-                  <a className="block mb-2 text-lg text-secondary">
-                    {subItem.label}
-                  </a>
-                </Link>
-              ))}
-            </Dropdown>
-          </div>
-        </div> */}
       </NavbarDropdown>
-    </div>
-  )
-}
-
-const Dropdown = ({
-  expanded,
-  children,
-}: {
-  expanded: boolean
-  children: React.ReactNode
-}) => {
-  const ref = React.useRef<HTMLDivElement>(null)
-  const scrollHeight = ref.current?.scrollHeight
-
-  return (
-    <div
-      ref={ref}
-      className="overflow-hidden transition-all duration-500"
-      style={expanded ? { maxHeight: scrollHeight } : { maxHeight: 0 }}
-    >
-      <div className="mt-4">{children}</div>
     </div>
   )
 }
--- apps/marketing/src/components/pages/HomePage/HomePageSimpleFeatureSection.tsx
+++ apps/marketing/src/components/pages/HomePage/HomePageSimpleFeatureSection.tsx
@@ -115,85 +115,87 @@ const HomePageSimpleFeatureSection = () => {
           that build brand engagement and loyalty.
         </p>
       </div>
-      {sections.map((section, index) => (
-        <div key={section.id} className="relative mt-12 sm:mt-16 lg:mt-24">
-          <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
-            <div
-              className={cx({
-                'lg:order-2': index % 2 === 0,
-              })}
-            >
-              <h3 className="text-2xl font-bold text-gray-900 tracking-tight sm:text-3xl">
-                {section.name}
-              </h3>
-              <p className="mt-3 text-lg text-gray-500">
-                {section.description}
-              </p>
-
-              <dl className="mt-10 space-y-10">
-                {section.features.map(item => (
-                  <div key={item.id} className="relative">
-                    <dt>
-                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                        <item.icon className="h-6 w-6" aria-hidden="true" />
-                      </div>
-                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                        {item.name}
-                      </p>
-                    </dt>
-                    <dd className="mt-2 ml-16 text-base text-gray-500">
-                      {item.description}
-                    </dd>
-                  </div>
-                ))}
-              </dl>
-            </div>
-
-            <div className="mt-10 -mx-4 relative lg:mt-0">
-              <svg
-                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
-                width={784}
-                height={404}
-                fill="none"
-                viewBox="0 0 784 404"
-                aria-hidden="true"
+      <div className="grid grid-cols-1 gap-10">
+        {sections.map((section, index) => (
+          <div key={section.id} className="relative mt-12 sm:mt-16 lg:mt-24">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+              <div
+                className={cx({
+                  'lg:order-2': index % 2 === 0,
+                })}
               >
-                <defs>
-                  <pattern
-                    id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
-                    x={0}
-                    y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect
-                      x={0}
-                      y={0}
-                      width={4}
-                      height={4}
-                      className="text-gray-200"
-                      fill="currentColor"
-                    />
-                  </pattern>
-                </defs>
-                <rect
+                <h3 className="text-2xl font-bold text-gray-900 tracking-tight sm:text-3xl">
+                  {section.name}
+                </h3>
+                <p className="mt-3 text-lg text-gray-500">
+                  {section.description}
+                </p>
+
+                <dl className="mt-10 space-y-10">
+                  {section.features.map(item => (
+                    <div key={item.id} className="relative">
+                      <dt>
+                        <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                          <item.icon className="h-6 w-6" aria-hidden="true" />
+                        </div>
+                        <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                          {item.name}
+                        </p>
+                      </dt>
+                      <dd className="mt-2 ml-16 text-base text-gray-500">
+                        {item.description}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+
+              <div className="mt-10 -mx-4 relative lg:mt-0">
+                <svg
+                  className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
                   width={784}
                   height={404}
-                  fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)"
-                />
-              </svg>
-              <div className="block items-center justify-center h-[490px] max-w-[600px] rounded-md bg-paper p-20">
-                <Image
-                  {...section.icon}
-                  layout="responsive"
-                  alt={`${section.name} icon`}
-                />
+                  fill="none"
+                  viewBox="0 0 784 404"
+                  aria-hidden="true"
+                >
+                  <defs>
+                    <pattern
+                      id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-200"
+                        fill="currentColor"
+                      />
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={784}
+                    height={404}
+                    fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)"
+                  />
+                </svg>
+                <div className="block items-center justify-center max-h-[490px] max-w-[600px] rounded-md bg-paper p-20">
+                  <Image
+                    {...section.icon}
+                    layout="responsive"
+                    alt={`${section.name} icon`}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </Section>
   )
 }
