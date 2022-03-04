5f476432561f33455096eed24708c4a2ed60515d
Everest Guerra <everest@umich.edu>
2022-03-04 09:01:19 -0500

    add copy to blog subscribe cta

--- apps/marketing/src/components/common/InlineMailingListSubscribe/InlineMailingListSubscribe.tsx
+++ apps/marketing/src/components/common/InlineMailingListSubscribe/InlineMailingListSubscribe.tsx
@@ -8,11 +8,11 @@ const InlineMailingListSubscribe = (props: InlineMailingListSubscribeProps) => {
     <div className="py-10 px-6 bg-indigo-700 rounded-3xl sm:py-16 sm:px-12 lg:p-20 lg:flex lg:items-center bg-primaryAlt-600">
       <div className="lg:w-0 lg:flex-1">
         <h2 className="text-3xl font-extrabold tracking-tight text-white">
-          Sign up for our newsletter
+          Become an audience growth & revenue expert
         </h2>
         <p className="mt-4 max-w-3xl text-lg text-white">
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem
-          cupidatat commodo. Elit sunt amet fugiat.
+          All the news and insights marketing pros need to know, all in one
+          newsletter. Stay informed and entertained, for free.
         </p>
       </div>
       <div className="mt-12 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 lg:flex-1">
