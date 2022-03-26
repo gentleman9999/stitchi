ddb1fd84163451300e7ec09185029d7b7526028f
Everest Guerra <everest@umich.edu>
2022-03-26 16:47:52 -0400

    fix build

--- apps/marketing/src/pages/catalog/index.tsx
+++ apps/marketing/src/pages/catalog/index.tsx
@@ -7,36 +7,37 @@ import React, { ReactElement } from 'react'
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const api = makeApi({ ctx })
 
-  const { _metadata, records } = await api.product.list()
+  // const { _metadata, records } = await api.product.list()
 
   return {
     props: {
-      records,
-      metadata: _metadata,
+      // records,
+      // metadata: _metadata,
     },
   }
 }
 
-const Catalog = (
-  props: InferGetServerSidePropsType<typeof getServerSideProps>,
-) => {
-  return (
-    <CatalogIndexPage
-      initialProducts={
-        props.records?.map(record => ({
-          ...record,
-          name: record.name || '',
-          image: record.image,
-          ratings: {
-            quality: 3,
-            softness: 1,
-            weight: 2,
-          },
-        })) || []
-      }
-    />
-  )
-}
+const Catalog = () =>
+  // props: InferGetServerSidePropsType<typeof getServerSideProps>,
+  {
+    return null
+    // return (
+    //   <CatalogIndexPage
+    //     initialProducts={
+    //       props.records?.map(record => ({
+    //         ...record,
+    //         name: record.name || '',
+    //         image: record.image,
+    //         ratings: {
+    //           quality: 3,
+    //           softness: 1,
+    //           weight: 2,
+    //         },
+    //       })) || []
+    //     }
+    //   />
+    // )
+  }
 
 Catalog.getLayout = (page: ReactElement) => (
   <PrimaryLayout>{page}</PrimaryLayout>
