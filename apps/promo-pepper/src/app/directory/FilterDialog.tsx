af0e6160b58722e750c654893a141570305ed560
Everest Guerra <everestguerra@Everests-MacBook-Pro.local>
2023-03-08 17:20:58 -0500

    stash

--- apps/promo-pepper/src/app/directory/FilterDialog.tsx
+++ apps/promo-pepper/src/app/directory/FilterDialog.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
-import { Container } from '@/components/ui'
 import { XIcon } from 'icons'
+import { Button } from '@/components/ui'
 
 interface Props {
   open: boolean
@@ -15,7 +15,7 @@ export default function FilterDialog({ open, onOpenChange }: Props) {
 
   return (
     <Dialog.Root modal open={open} onOpenChange={onOpenChange}>
-      <Dialog.Portal className="">
+      <Dialog.Portal className="z-50">
         <Dialog.Overlay className="fixed top-0 left-0 bottom-0 right-0 bg-gray-900 opacity-10 grid place-items-center overflow-y-auto" />
         {/* <Container> */}
         <div className="fixed top-0 left-0 bottom-0 right-0 grid place-items-center overflow-y-auto">
@@ -25,11 +25,27 @@ export default function FilterDialog({ open, onOpenChange }: Props) {
                 <Dialog.Close className="absolute top-0 left-0">
                   <XIcon height={24} />
                 </Dialog.Close>
-                <Dialog.Title className="text-2xl">Filters</Dialog.Title>
+                <Dialog.Title className="text-xl font-semibold font-heading text-gray-700">
+                  Filters
+                </Dialog.Title>
               </div>
             </Section>
             <hr />
-            <Section>New section</Section>
+            <Section>
+              <SectionTitle title="Supply chain" />
+              <br />
+              <CheckboxFilterGroup name="Supply chain" />
+            </Section>
+            <hr />
+            <Section>
+              <SectionTitle title="Product types" />
+              <br />
+              <CheckboxFilterGroup name="Product types" />
+            </Section>
+            <hr />
+            <Section>
+              <Button>Show x+ products</Button>
+            </Section>
           </Dialog.Content>
         </div>
 
@@ -40,5 +56,96 @@ export default function FilterDialog({ open, onOpenChange }: Props) {
 }
 
 const Section = ({ children }: { children: React.ReactNode }) => (
-  <div className="p-4">{children}</div>
+  <div className="p-6">{children}</div>
 )
+
+const SectionTitle = ({
+  title,
+  subtitle,
+}: {
+  title: string
+  subtitle?: string
+}) => {
+  return (
+    <div className="flex flex-col gap-2 font-heading">
+      <h2 className="text-xl font-semibold">{title}</h2>
+      {subtitle && <p className="text-sm">{subtitle}</p>}
+    </div>
+  )
+}
+
+const CheckboxFilterGroup = ({ name }: { name: string }) => {
+  return (
+    <fieldset>
+      <legend className="sr-only">{name}</legend>
+      <div className="space-y-5">
+        <div className="relative flex items-start">
+          <div className="flex h-6 items-center">
+            <input
+              id="comments"
+              aria-describedby="comments-description"
+              name="comments"
+              type="checkbox"
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            />
+          </div>
+          <div className="ml-3">
+            <label
+              htmlFor="comments"
+              className="text-sm font-medium leading-6 text-gray-900"
+            >
+              Comments
+            </label>
+            <p id="comments-description" className="text-sm text-gray-500">
+              Get notified when someones posts a comment on a posting.
+            </p>
+          </div>
+        </div>
+        <div className="relative flex items-start">
+          <div className="flex h-6 items-center">
+            <input
+              id="candidates"
+              aria-describedby="candidates-description"
+              name="candidates"
+              type="checkbox"
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            />
+          </div>
+          <div className="ml-3">
+            <label
+              htmlFor="candidates"
+              className="text-sm font-medium leading-6 text-gray-900"
+            >
+              Candidates
+            </label>
+            <p id="candidates-description" className="text-sm text-gray-500">
+              Get notified when a candidate applies for a job.
+            </p>
+          </div>
+        </div>
+        <div className="relative flex items-start">
+          <div className="flex h-6 items-center">
+            <input
+              id="offers"
+              aria-describedby="offers-description"
+              name="offers"
+              type="checkbox"
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            />
+          </div>
+          <div className="ml-3">
+            <label
+              htmlFor="offers"
+              className="text-sm font-medium leading-6 text-gray-900"
+            >
+              Offers
+            </label>
+            <p id="offers-description" className="text-sm text-gray-500">
+              Get notified when a candidate accepts or rejects an offer.
+            </p>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  )
+}
--- apps/promo-pepper/src/components/ui/index.ts
+++ apps/promo-pepper/src/components/ui/index.ts
@@ -1,3 +1,7 @@
+export { default as Button } from './Button'
+export type { Props as ButtonProps } from './Button'
+
 export { default as Container } from './Container'
+export { default as LoadingDots } from './LoadingDots'
 export { default as Logo } from './Logo'
 export { default as Skeleton } from './Skeleton'
