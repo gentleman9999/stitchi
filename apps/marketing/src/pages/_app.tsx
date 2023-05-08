d8699fd59beff2621d524f338a16531ede99c1f9
Everest Guerra <everestguerra@Everests-MacBook-Pro-2.local>
2023-05-08 12:57:48 -0400

    more updates to test

--- apps/marketing/src/pages/_app.tsx
+++ apps/marketing/src/pages/_app.tsx
@@ -31,19 +31,6 @@ const Page = ({ Component, pageProps }: ExtendedAppProps) => {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? (page => page)
 
-  // /**
-  //  * Chrome has a bug with transitions on load since 2012!
-  //  *
-  //  * To prevent a "pop" of content, you have to disable all transitions until
-  //  * the page is done loading.
-  //  *
-  //  * https://lab.laukstein.com/bug/input
-  //  * https://twitter.com/timer150/status/1345217126680899584
-  //  */
-  // useEffect(() => {
-  //   document.body.classList?.remove('loading')
-  // }, [])
-
   return (
     <div className={`${inter.variable}`}>
       <ApolloProvider client={apolloClient}>
--- apps/marketing/tailwind.config.js
+++ apps/marketing/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
     fontFamily: {
       heading: ['Degular'],
       headingDisplay: ['Degular-Display'],
-      default: ['var(--font-inter)'],
+      default: ['var(--font-inter)', 'sans-serif'],
     },
     colors: {
       transparent: 'transparent',
