a8fcc01fa5e9a1834564eb9acd287e3984052054
gentleman9999 <pamy36693@gmail.com>
2023-07-10 09:16:22 -0400

    updates bigc product description script

--- apps/server/scripts/generate-product-descriptions.ts
+++ apps/server/scripts/generate-product-descriptions.ts
@@ -1,8 +1,15 @@
 import { exit } from 'process'
 import fetch, { RequestInit } from 'node-fetch'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
-import { HumanChatMessage, SystemChatMessage } from 'langchain/schema'
+// import { HumanChatMessage, SystemChatMessage } from 'langchain/schema'
+import {
+  StructuredOutputParser,
+  OutputFixingParser,
+} from 'langchain/output_parsers'
+import { PromptTemplate } from 'langchain/prompts'
+import { LLMChain } from 'langchain/chains'
 import { isAfter, parseISO } from 'date-fns'
+import { z } from 'zod'
 import { getOrThrow } from '../src/utils'
 
 const LAST_UPDATED_AT_LIMIT = getOrThrow(
@@ -12,14 +19,38 @@ const LAST_UPDATED_AT_LIMIT = getOrThrow(
 
 const updatedAtFieldKey = 'updated_description_at'
 
-const chat = new ChatOpenAI({
+const chatModel = new ChatOpenAI({
   maxTokens: 500,
-  n: 1,
-  temperature: 0.8,
+  temperature: 0,
   modelName: 'gpt-3.5-turbo',
   openAIApiKey: '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',
   timeout: 45_000, // 45 seconds
   maxRetries: 3,
+})
+
+const chatOutputParser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    text: z.string().describe('The description of the product'),
+  }),
+)
+
+const chatOutputFixingParser = OutputFixingParser.fromLLM(
+  chatModel,
+  chatOutputParser,
+)
+
+const chatPrompt = new PromptTemplate({
+  template: `Write an SEO-optimized product description:\n{format_instructions}\nproduct name:{name}\n product brand:{brand} \n currentDescription:{description}`,
+  inputVariables: ['name', 'brand', 'description'],
+  partialVariables: {
+    format_instructions: chatOutputFixingParser.getFormatInstructions(),
+  },
+})
+
+const chatFormattingChain = new LLMChain({
+  llm: chatModel,
+  prompt: chatPrompt,
+  outputParser: chatOutputFixingParser,
 })
 
 const bigCommerceApiPath = 'https://api.bigcommerce.com/stores/ycjcgspsys/v3'
@@ -39,9 +70,13 @@ const fetcher = (path: string, init?: RequestInit) => {
 async function start() {
   console.log('Generating product descriptions...')
 
-  let totalPages: number | null = 1
   let page = 1
   const limit = 10
+
+  let totalPages: number | null = 1
+
+  let productCount = 0
+  let totalProductCount = 0
 
   while (totalPages === null || page <= totalPages) {
     const productsResponse = await fetcher(
@@ -53,7 +88,7 @@ async function start() {
         id?: string
         name?: string
         description?: string
-        brandId?: string
+        brand_id?: string
         custom_fields?: { id: string; name: string; value: string }[]
       }[]
       meta?: {
@@ -68,9 +103,12 @@ async function start() {
 
     const products = json.data || []
 
+    productCount += products.length
+    totalProductCount = totalPages ? totalPages * limit : 0
+
     const productPromises = products.map(async product => {
       const productName = product?.name
-      const brandId = product?.brandId
+      const brandId = product?.brand_id
       const updatedAtField = product?.custom_fields?.find(
         field => field.name === updatedAtFieldKey,
       )
@@ -98,14 +136,30 @@ async function start() {
           }
         }
 
-        const description = await generateNewDescription(
-          productName,
-          product?.description,
-          brand?.name,
-        )
+        let description
+        try {
+          const response = await chatFormattingChain.call({
+            name: productName,
+            brand: brand?.name || '',
+            description: product?.description || '',
+          })
+
+          description = response?.text?.text
+        } catch (error) {
+          console.log(error)
+          return
+        }
 
         if (description) {
           console.log(`Updating ${productName} description...`)
+
+          if (process.env.SKIP_PRODUCT_UPDATES === 'true') {
+            console.log(
+              'DESCRIPTION WAS GENERATED BUT NOT UPDATED: ',
+              description,
+            )
+            return
+          }
 
           await fetcher(`/catalog/products/${product.id}`, {
             method: 'PUT',
@@ -127,45 +181,51 @@ async function start() {
     })
 
     await Promise.all(productPromises)
+
+    console.log('---------------------------------')
+    console.log(
+      `Generated descriptions for ${productCount} of ${totalProductCount} products...`,
+    )
+    console.log('---------------------------------')
   }
 }
 
-const generateNewDescription = async (
-  productName: string,
-  brandName?: string,
-  productDescription?: string,
-) => {
-  const response = await chat.call([
-    new SystemChatMessage(
-      'You are a promotional-product catalog manager that specializes in writing SEO optimized product descriptions. You should respond to all messages with the following format: "[insert-description]"',
-    ),
-    new HumanChatMessage(
-      `Write an SEO optimized description for the product "${productName}" ${
-        brandName ? `from brand "${brandName}"` : ''
-      } with the existing description "${productDescription || ''}".`,
-    ),
-    new HumanChatMessage(
-      `Include the following sections: Product overview, Use cases, Customization options (choose between screen print, DTG, embroidery, etc. based on product type), and Product specs.).`,
-    ),
-    new HumanChatMessage(`Write in 2nd person (you = the customer).`),
-    new HumanChatMessage(`Example: <div>
-    <h2>Product Overview</h2>
-    <p>Stay hydrated and eco-friendly with the HydraPeak Insulated Stainless Steel Water Bottle. This sleek and durable bottle is perfect for everyday use, outdoor adventures, and sports activities.</p>
-    <h2>Features & Benefits</h2>
-    <ul>
-        <li>Double-wall vacuum insulation keeps beverages cold for 24 hours and hot for 12 hours</li>
-        <li>Constructed from high-quality 18/8 food-grade stainless steel</li>
-        <li>Leak-proof lid with a convenient carry handle</li>
-        <li>BPA-free and eco-friendly</li>
-        <li>Available in multiple colors and sizes (18oz, 24oz, 32oz)</li>
-    </ul>
-    <h2>Customization Options</h2>
-    <p>Customize your HydraPeak bottle with laser engraving or screen printing for a unique promotional item or personalized gift.</p>
-    </div>`),
-  ])
+// const generateNewDescription = async (
+//   productName: string,
+//   brandName?: string,
+//   productDescription?: string,
+// ) => {
+//   const response = await chatModel.call([
+//     new SystemChatMessage(
+//       'You are a promotional-product catalog manager that specializes in writing SEO optimized product descriptions. You should respond to all messages with the following format: "[insert-description]"',
+//     ),
+//     new HumanChatMessage(
+//       `Write an SEO optimized description for the product "${productName}" ${
+//         brandName ? `from brand "${brandName}"` : ''
+//       } with the existing description "${productDescription || ''}".`,
+//     ),
+//     new HumanChatMessage(
+//       `Include the following sections: Product overview, Use cases, Customization options (choose between screen print, DTG, embroidery, etc. based on product type), and Product specs.).`,
+//     ),
+//     new HumanChatMessage(`Write in 2nd person (you = the customer).`),
+//     new HumanChatMessage(`Example: <div>
+//     <h2>Product Overview</h2>
+//     <p>Stay hydrated and eco-friendly with the HydraPeak Insulated Stainless Steel Water Bottle. This sleek and durable bottle is perfect for everyday use, outdoor adventures, and sports activities.</p>
+//     <h2>Features & Benefits</h2>
+//     <ul>
+//         <li>Double-wall vacuum insulation keeps beverages cold for 24 hours and hot for 12 hours</li>
+//         <li>Constructed from high-quality 18/8 food-grade stainless steel</li>
+//         <li>Leak-proof lid with a convenient carry handle</li>
+//         <li>BPA-free and eco-friendly</li>
+//         <li>Available in multiple colors and sizes (18oz, 24oz, 32oz)</li>
+//     </ul>
+//     <h2>Customization Options</h2>
+//     <p>Customize your HydraPeak bottle with laser engraving or screen printing for a unique promotional item or personalized gift.</p>
+//     </div>`),
+//   ])
 
-  return response.text
-}
+//   return response.text
+// }
 
 start()
   .catch(e => console.error(e))
