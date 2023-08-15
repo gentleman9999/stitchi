8c1062f3449a216ebb90baadf76fad595f9c4e5a
gentleman9999 <pamy36693@gmail.com>
2023-08-14 23:53:52 -0400

    prxy

--- apps/marketing/next.config.js
+++ apps/marketing/next.config.js
@@ -31,8 +31,8 @@ module.exports = withBundleAnalyzer({
         destination: 'https://stitchiblog.wpengine.com/blog/:path*',
       },
       {
-        source: '/blog/wp-admin/:path*',
-        destination: 'https://stitchiblog.wpengine.com/blog/wp-admin/:path*/',
+        source: '/blog/wp-admin:path*',
+        destination: 'https://stitchiblog.wpengine.com/blog/wp-admin:path*',
       },
     ]
   },
