53907179bda100428a2999536300d9698c5de88e
gentleman9999 <pamy36693@gmail.com>
2023-09-11 12:59:20 -0700

    feature complete?

--- apps/server/src/services/notification/templates/index.ts
+++ apps/server/src/services/notification/templates/index.ts
@@ -1,7 +1,13 @@
 import { User } from 'auth0'
+import { getOrThrow } from '../../../utils'
 import { DesignFactoryDesignRequest } from '../../design/factory'
 import { MembershipFactoryMembership } from '../../membership/factory/membership'
 import { OrderFactoryOrder } from '../../order/factory'
+
+const appBaseUrl = getOrThrow(
+  process.env.STITCHI_MARKETING_APPLICATION_HOST,
+  'STITCHI_MARKETING_APPLICATION_HOST',
+)
 
 interface Notification {
   email: {
@@ -30,8 +36,8 @@ const notifications = {
       },
       web: {
         message: `Your order #${params.order.humanReadableId} is confirmed.`,
-        ctaText: null,
-        ctaUrl: null,
+        ctaText: 'View',
+        ctaUrl: `${appBaseUrl}/orders/${params.order.id}`,
       },
     }
   },
@@ -49,8 +55,27 @@ const notifications = {
       },
       web: {
         message: `Your design request ${params.designRequest.name} has been submitted and will be reviewed by an artist shortly.`,
-        ctaText: null,
-        ctaUrl: null,
+        ctaText: 'View',
+        ctaUrl: `${appBaseUrl}/closet/design-requests/${params.designRequest.id}`,
+      },
+    }
+  },
+
+  'designRequestProof:created': (params: {
+    designRequest: DesignFactoryDesignRequest
+    membership: MembershipFactoryMembership
+    user: User
+  }): Notification => {
+    return {
+      email: {
+        subject: 'A design proof was submitted!',
+        htmlBody: `A design proof was submitted for your design request ${params.designRequest.name}.`,
+        textBody: `A design proof was submitted for your design request ${params.designRequest.name}.`,
+      },
+      web: {
+        message: `A design proof was submitted for your design request ${params.designRequest.name}.`,
+        ctaText: 'View',
+        ctaUrl: `${appBaseUrl}/closet/design-requests/${params.designRequest.id}`,
       },
     }
   },
