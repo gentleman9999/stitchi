9345d383c2b2351c754c705eec829ae80e452e6b
custompro98 <mitchjoa@gmail.com>
2023-10-05 20:20:36 -0400

    Build up permission map

--- apps/server/src/graphql/authorization.ts
+++ apps/server/src/graphql/authorization.ts
@@ -6,6 +6,45 @@ type ScopeResource = NexusGenEnums['ScopeResource']
 type ScopeAction = NexusGenEnums['ScopeAction']
 type ScopeModifier = NexusGenEnums['ScopeModifier']
 
+export const scopeMap: Record<
+  NexusGenEnums['MembershipRole'],
+  Record<
+    NexusGenEnums['ScopeResource'],
+    [NexusGenEnums['ScopeAction'], NexusGenEnums['ScopeModifier']?][]
+  >
+> = {
+  OWNER: {
+    Order: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+    DesignProof: [['READ', 'OWN']],
+    DesignProduct: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+    DesignRequest: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+    DesignRequestRevisionRequest: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+    Integration: [['CREATE'], ['READ'], ['UPDATE'], ['DELETE']],
+    Membership: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+    Organization: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+  },
+  STITCHI_DESIGNER: {
+    Order: [],
+    DesignProof: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
+    DesignProduct: [],
+    DesignRequest: [['READ', 'ALL']],
+    DesignRequestRevisionRequest: [['READ', 'OWN']],
+    Integration: [],
+    Membership: [],
+    Organization: [],
+  },
+  STITCHI_ADMIN: {
+    Order: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    DesignProof: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    DesignProduct: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    DesignRequest: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    DesignRequestRevisionRequest: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    Integration: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    Membership: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+    Organization: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
+  },
+}
+
 interface AuthorizerParams {
   modifier: ScopeModifier
 }
@@ -27,6 +66,24 @@ type PermissionMap = {
 
 export function makeAuthorizer(role: Role): AuthorizerFn {
   const permissionMap: PermissionMap = {}
+
+  for (const [resource, scopePairs] of Object.entries(scopeMap[role])) {
+    if (!permissionMap[resource]) {
+      // If there is no configuration yet for the resource, set it up
+      permissionMap[resource] = {}
+    }
+
+    for (const [action, modifier] of scopePairs) {
+      if (!permissionMap[resource][action]) {
+        // If we haven't seen this scope yet, assign it
+        permissionMap[resource][action] = modifier
+      } else if (modifier === 'ALL') {
+        // If we've seen the scope and the new modifier is 'ALL',
+        // overwrite it. If the previous is 'ALL', it's a no-op.
+        permissionMap[resource][action] = modifier
+      }
+    }
+  }
 
   return function(action: ScopeAction, resource: ScopeResource, params: AuthorizerParams) {
     const permissionFound = resource in permissionMap && action in permissionMap[resource]
--- apps/server/src/graphql/queries/scope/scope.ts
+++ apps/server/src/graphql/queries/scope/scope.ts
@@ -1,44 +1,6 @@
 import { extendType } from 'nexus'
+import { scopeMap } from '../../authorization'
 import { NexusGenObjects, NexusGenEnums } from '../../generated/nexus'
-
-export const scopeMap: Record<
-  NexusGenEnums['MembershipRole'],
-  Record<
-    NexusGenEnums['ScopeResource'],
-    [NexusGenEnums['ScopeAction'], NexusGenEnums['ScopeModifier']?][]
-  >
-> = {
-  OWNER: {
-    Order: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-    DesignProof: [['READ', 'OWN']],
-    DesignProduct: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-    DesignRequest: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-    DesignRequestRevisionRequest: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-    Integration: [['CREATE'], ['READ'], ['UPDATE'], ['DELETE']],
-    Membership: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-    Organization: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-  },
-  STITCHI_DESIGNER: {
-    Order: [],
-    DesignProof: [['CREATE'], ['READ', 'OWN'], ['UPDATE', 'OWN'], ['DELETE', 'OWN']],
-    DesignProduct: [],
-    DesignRequest: [['READ', 'ALL']],
-    DesignRequestRevisionRequest: [['READ', 'OWN']],
-    Integration: [],
-    Membership: [],
-    Organization: [],
-  },
-  STITCHI_ADMIN: {
-    Order: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    DesignProof: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    DesignProduct: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    DesignRequest: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    DesignRequestRevisionRequest: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    Integration: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    Membership: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-    Organization: [['CREATE'], ['READ', 'ALL'], ['UPDATE', 'ALL'], ['DELETE', 'ALL']],
-  },
-}
 
 export const ScopeExtendsMembership = extendType({
   type: 'Membership',
