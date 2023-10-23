b74d2613c07315bceacecfe0122cacb5573b9aed
gentleman9999 <pamy36693@gmail.com>
2023-10-23 19:55:32 -0400

    feat(app): reduce middleware requests

--- apps/marketing/src/middleware.ts
+++ apps/marketing/src/middleware.ts
@@ -87,4 +87,17 @@ const middleware: NextMiddleware = async (request, event) => {
   return response
 }
 
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
+
 export default middleware
