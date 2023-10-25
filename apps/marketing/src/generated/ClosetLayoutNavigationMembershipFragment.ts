d28d94d824aeb05eae9475634ebfbf52027af35b
gentleman9999 <pamy36693@gmail.com>
2023-10-25 18:06:29 -0400

    feat(app): nue nav

---
+++ apps/marketing/src/generated/ClosetLayoutNavigationMembershipFragment.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: ClosetLayoutNavigationMembershipFragment
+// ====================================================
+
+export interface ClosetLayoutNavigationMembershipFragment {
+  __typename: "Membership";
+  id: string;
+  unseenWebNotificationsCount: number;
+}
