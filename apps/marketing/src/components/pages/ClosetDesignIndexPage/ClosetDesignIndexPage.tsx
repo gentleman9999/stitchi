2a257d6b365e7ab3bf683014572182dd0d64287f
gentleman9999 <pamy36693@gmail.com>
2023-10-25 23:34:24 -0400

    stash onboarding banners

--- apps/marketing/src/components/pages/ClosetDesignIndexPage/ClosetDesignIndexPage.tsx
+++ apps/marketing/src/components/pages/ClosetDesignIndexPage/ClosetDesignIndexPage.tsx
@@ -6,6 +6,7 @@ import ClosetPageTitle from '@components/common/ClosetPageTitle'
 import ClosetSection from '@components/common/ClosetSection'
 import ClosetSectionHeader from '@components/common/ClosetSectionHeader'
 import ClosetSectionHeaderTabs from '@components/common/ClosetSectionHeaderTabs'
+import Button from '@components/ui/ButtonV2/Button'
 import { ClosetDesignIndexPageGetDataQuery } from '@generated/ClosetDesignIndexPageGetDataQuery'
 import {
   MembershipRole,
@@ -66,6 +67,26 @@ const ClosetDesignIndexPage = ({}: Props) => {
             }
           />
         </ClosetPageHeader>
+
+        <ClosetSection>
+          <div className="bg-paper border rounded-md p-6">
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col gap-2">
+                <h2 className="font-md">Welcome to your design hub</h2>
+                <p className="text-gray-700 text-sm max-w-2xl">
+                  Your designs are the creative blueprints for your products.
+                  Here you&apos;ll work with a designer to bring your ideas to
+                  life. Finished designs will be available in your inventory to
+                  send around the world.
+                </p>
+              </div>
+
+              <Button variant="ghost" size="xl">
+                Got it
+              </Button>
+            </div>
+          </div>
+        </ClosetSection>
 
         <ClosetDesignFilters />
 
--- apps/marketing/src/components/pages/ClosetInventoryIndexPage/ClosetInventoryIndexPage.tsx
+++ apps/marketing/src/components/pages/ClosetInventoryIndexPage/ClosetInventoryIndexPage.tsx
@@ -2,6 +2,7 @@ import ClosetPageContainer from '@components/common/ClosetPageContainer'
 import ClosetPageHeader from '@components/common/ClosetPageHeader'
 import ClosetPageTitle from '@components/common/ClosetPageTitle'
 import ClosetSection from '@components/common/ClosetSection'
+import Button from '@components/ui/ButtonV2/Button'
 import React from 'react'
 import ClosetInventoryIndexPageInventoryList from './ClosetInventoryIndexPageInventoryList'
 
@@ -13,6 +14,25 @@ const ClosetInventoryIndexPage = (props: Props) => {
       <ClosetPageHeader>
         <ClosetPageTitle title="Inventory" description="" />
       </ClosetPageHeader>
+
+      <ClosetSection>
+        <div className="bg-paper border rounded-md p-6">
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col gap-2">
+              <h2 className="font-md">All your inventory in one place</h2>
+              <p className="text-gray-700 text-sm max-w-2xl">
+                Your inventory are all of the items stored with Stitchi. You can
+                easily send items from your inventory anywhere, or use one of
+                our integrations to auto-magically fulfill!
+              </p>
+            </div>
+
+            <Button variant="ghost" size="xl">
+              Got it
+            </Button>
+          </div>
+        </div>
+      </ClosetSection>
 
       <ClosetSection>
         <ClosetInventoryIndexPageInventoryList />
