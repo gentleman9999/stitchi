aea6544fe8adc256d0297eb0e1a483bd4f9d18ea
gentleman9999 <pamy36693@gmail.com>
2023-11-10 13:09:54 -0500

    remove manual smartlook in favor of GTM

--- apps/marketing/src/pages/_document.tsx
+++ apps/marketing/src/pages/_document.tsx
@@ -1,12 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { GTM_ID } from '@lib/events'
-import Script from 'next/script'
-import getOrThrow from '@lib/utils/get-or-throw'
-
-const smartlookApiKey = getOrThrow(
-  process.env.NEXT_PUBLIC_SMARTLOOK_API_KEY,
-  'NEXT_PUBLIC_SMARTLOOK_API_KEY',
-)
 
 export default class MyDocument extends Document {
   render() {
@@ -14,23 +7,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="stylesheet" href="https://use.typekit.net/msx0isz.css" />
-
-          {/* Smartlook */}
-          <Script
-            id="smarlook-global"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.smartlook||(function(d) {
-            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-            })(document);
-            smartlook('init', '${smartlookApiKey}', { region: 'eu' });
-          `,
-            }}
-          />
-          {/* Smartlook - end */}
         </Head>
         <body>
           <noscript>
