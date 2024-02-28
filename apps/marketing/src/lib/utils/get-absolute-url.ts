e3dda0fd4d94ebfc7e9b2254ca475de5e1261632
gentleman9999 <pamy36693@gmail.com>
2024-02-28 14:20:09 -0500

    fix home page indexability

--- apps/marketing/src/lib/utils/get-absolute-url.ts
+++ apps/marketing/src/lib/utils/get-absolute-url.ts
@@ -1,5 +1,9 @@
 import { SITE_URL } from '@lib/constants'
 
-const makeAbsoluteUrl = (url: string) => `${SITE_URL}${url}`
+const makeAbsoluteUrl = (url: string) => {
+  // Remove the last character if it's a trailing slash
+  const sanitizedUrl = url.endsWith('/') ? url.slice(0, -1) : url
+  return `${SITE_URL}${sanitizedUrl}`
+}
 
 export default makeAbsoluteUrl
